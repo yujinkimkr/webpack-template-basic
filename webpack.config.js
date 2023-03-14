64f5e1afd2a3f5e2fc2ee702155a2f704a7e2dfa
yujinkimkr <kyjin.dv@gmail.com>
2023-03-14 22:44:20 +0100

    create project

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+// export
+module.exports = {
+    // parcel index.html
+    // 파일을 읽어들이기 시작하는 진입점 설정
+    // webpack은 html이 아닌 js를 진입점으로 설정한다
+    entry: './js/main.js',
+    
+    // 결과물(번들)을 반환하는 설정
+    output: {
+        // 절대경로가 필요함 - dirname 현재 파일이 있는 그 경로
+        // path: path.resolve(__dirname, 'dist'),
+        // filename: 'main.js',
+        clean: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/, 
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
+    plugins: [
+        new HtmlPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'static'}
+            ]
+        })
+    ],
+
+    devServer: {
+        host: 'localhost'
+    }
+}
